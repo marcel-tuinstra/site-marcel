9bae26313cc308d51ed7b7b64037e39e337a1d33
Marcel Tuinstra <marcel@codeducks.com>
2025-04-19 00:25:47 +0200

    [GITHUB] Disabled NuxtStudio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
     modules: ['@nuxt/content', '@nuxt/ui'],
-    css: ['~/assets/css/main.css']
+    css: ['~/assets/css/main.css'],
+    content: {
+        studio: {
+            enabled: false // explicitly disable
+        }
+    }
 })
