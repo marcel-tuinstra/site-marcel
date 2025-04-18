9e4b6c38f49da00c03e16e96e999b87befac985a
Marcel Tuinstra <marcel@codeducks.com>
2025-04-18 23:14:32 +0200

    [GITHUB] Restart

--- components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <footer class="bg-gray-900 text-white py-4">
-        <UContainer>
-            <div class="text-center">
-                <p>© 2025 Marcel Tuinstra. All rights reserved.</p>
-            </div>
-        </UContainer>
-    </footer>
-</template>
--- content/index.md
+++
@@ -1,7 +0,0 @@
----
-title: "Hey, I’m Marcel"
-description: "Fullstack developer building clean UIs and modern sites"
----
-
-Welcome to my portfolio.  
-This content is pulled from markdown using Nuxt Content!
